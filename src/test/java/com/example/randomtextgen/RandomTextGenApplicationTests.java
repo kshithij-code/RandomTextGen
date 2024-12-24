22f0d3a927801b937d9fb0b7312d6f1f96b9bcef
kshithij <kshithijy@gmail.com>
2024-12-24 20:26:08 +0530

    init

---
+++ src/test/java/com/example/randomtextgen/RandomTextGenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.randomtextgen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RandomTextGenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
